616bea7fc3626d1af010931230947979a1495d4b
zlfn <ung@zlfn.space>
2024-09-12 01:51:50 +0900

    :recycle: Refactor recfun code

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -7,17 +7,11 @@ object Main {
     println("Pascal's Triangle")
     for (row <- 0 to 10) {
       for (col <- 0 to row)
-        print(pascal(col, row) + " ")
+        //method + in class Int is deprecated (2.13.0)
+        //print(pascal(col, row) + "")
+        print(s"${pascal(col, row)} ")
       println()
     }
-    println()
-
-    println("Parenthesis Balancing")
-    println(balance("(TEST)".toList))
-    println(balance("(TEST))".toList))
-    println()
-
-
   }
 
   /**
@@ -25,7 +19,7 @@ object Main {
    */
   def pascal(c: Int, r: Int): Int = (c, r) match {
     case (0, 0) => 1
-    case (c, r) if c < 0 || c > r => 0
+    case _ if c < 0 || c > r => 0
     case _ => pascal(c, r-1) + pascal(c-1, r-1)
   }
 
@@ -33,14 +27,12 @@ object Main {
    * Exercise 2
    */
   def balance(chars: List[Char]): Boolean = {
-    def recBalance(slice: List[Char], count: Int): Boolean = {
-      if (count < 0) { return false }
-      slice match {
-        case '('+:slice => recBalance(slice, count + 1)
-        case ')'+:slice => recBalance(slice, count - 1)
-        case _+:slice => recBalance(slice, count)
-        case _ => count == 0
-      }
+    def recBalance(slice: List[Char], count: Int): Boolean = slice match {
+      case _ if count < 0 => false
+      case '(' :: slice => recBalance(slice, count + 1)
+      case ')' :: slice => recBalance(slice, count - 1)
+      case _ :: slice => recBalance(slice, count)
+      case Nil => count == 0
     }
     recBalance(chars, 0)
   }
@@ -48,12 +40,10 @@ object Main {
   /**
    * Exercise 3
    */
-  def countChange(money: Int, coins: List[Int]): Int = {
-    if (money < 0) return 0
-    (money, coins) match {
-      case (0, _) => 1
-      case (_, coin+:tail) => countChange(money - coin, coins) + countChange(money, tail)
-      case _ => 0
-    }
+  def countChange(money: Int, coins: Seq[Int]): Int = (money, coins) match {
+    case _ if money < 0 => 0
+    case (0, _) => 1
+    case (_, Seq()) => 0
+    case (_, coin :: tail) => countChange(money - coin, coins) + countChange(money, tail)
   }
 }
